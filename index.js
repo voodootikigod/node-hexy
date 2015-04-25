6e53b538db0582b4f950f6c2bafe207211aa1733
Chris Williams <chris@iterativedesigns.com>
2015-04-25 15:55:09 -0400

    Update.

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ var Hexy = require('./lib/hexy');
 var TARGET = '/dev/tty.usbmodem1421';
 var hexy;
 
-hexy = new Hexy(TARGET, function () {});
+hexy = new Hexy(TARGET, function () {
+  hexy.zero( function () {
+    hexy.up(function () {
+      // hexy.stop(function () {
+        process.exit(1);
+      // });
+    });
+
+  });
+});
 
